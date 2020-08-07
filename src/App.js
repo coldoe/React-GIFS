1ca76cb06b917a5d31b8489e18409a9f55bfa77c
Iceberg <hawxhawx1920@gmail.com>
2020-08-07 13:43:08 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//components
+import { search } from "./Components/searchComponent/search";
+import { random } from "./Components/RandomComponent/random";
+import { main } from "./Components/MainComponent/main";
+//styles
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={main} />
+          <Route exact path="/random" component={random} />
+          <Route exact path="/search" component={search} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/RandomComponent/random.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const random = () => {
+  return (
+    <div>
+      <h1>random</h1>
+    </div>
+  );
+};
